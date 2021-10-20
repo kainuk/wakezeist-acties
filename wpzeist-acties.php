e3c1e13700f7b3f551fca6b29cc1ec94b908d0e1
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2021-10-20 23:17:27 +0200

    Intake

---
+++ wpzeist-acties.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WakeZeist Acties
+Plugin URI: https://github.com/kainuk/wakezeist-acties
+Description: Specifieke functionaliteit voor wakezeist.nl
+Version: 5.10.2
+Author: Klaas Eikelboom
+Author URI: https://www.eikelboom.com
+*/
