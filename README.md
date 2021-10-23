a830c37d1133bf0aff9b7bebcbe9972d630ba49e
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2021-10-23 15:18:09 +0200

    Toevoegen van de [volgende_wake] short code.

---
+++ README.md
@@ -0,0 +1,5 @@
+# WakeZeist acties
+
+## Short codes
+
+`[volgende_wake]` : Drukt de eerste zondag van de maand af.
--- wakezeist-acties.php
+++ wakezeist-acties.php
@@ -7,6 +7,11 @@ Version: 5.10.2
 Author: Klaas Eikelboom
 Author URI: https://www.eikelboom.com
 */
-add_shortcode('wakes', function(){
-  return '<p>Testresultaat Wakes (het is gelukt)</p>';
+add_shortcode('volgende_wake', function(){
+  $date = new DateTime();
+  $interval = DateInterval::createFromDateString('first sunday of next month');
+  $date->add($interval);
+  $fmt = new \IntlDateFormatter('nl_NL', NULL, NULL);
+  $fmt->setPattern('d MMMM yyyy');
+  return $fmt->format($date);
 });
