1f9e12bc0cb5a192f0a380c557d898ee37d57a81
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2021-10-24 20:04:12 +0200

    Refactoring and removing obsolete code.

--- wakezeist-acties.php
+++ wakezeist-acties.php
@@ -7,21 +7,18 @@ Version: 5.10.2
 Author: Klaas Eikelboom
 Author URI: https://www.eikelboom.com
 */
-require_once __DIR__.'/includes/WakeZeistHelper.php';
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+  die;
+}
+require_once __DIR__ . '/includes/WakeZeistHelper.php';
 
-add_shortcode('volgende_wake', function(){
+add_shortcode('volgende_wake', function () {
   return WakeZeistHelper::instance()->volgendeWake();
 });
 
-add_shortcode('vorige_jaren', function (){
-  $output = '';
-  for($year=2013;$year<=2021;$year++){
-    $output .= "<li>$year</li>";
-  }
-  return "<ul>$output</ul>";
-});
-add_shortcode('jaarindex', function ($atts){
-  $args = shortcode_atts(['jaar'=>2021],$atts);
+add_shortcode('jaarindex', function ($atts) {
+  $args = shortcode_atts(['jaar' => 2021], $atts);
   return WakeZeistHelper::instance()->jaarIndex($args['jaar']);
 });
 
